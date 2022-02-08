233254e704eec5ec5a78ad25ee506a97c621419a
lumanda <854260059@qq.com>
2022-02-08 16:43:44 +0800

    58,第二次提交

--- 58.最后一个单词的长度.js
+++ 58.最后一个单词的长度.js
@@ -3,7 +3,7 @@
  * @version: data
  * @Author: @zhanghuiyong
  * @Date: 2022-02-08 16:24:49
- * @LastEditTime: 2022-02-08 16:26:40
+ * @LastEditTime: 2022-02-08 16:43:22
  */
 /*
  * @lc app=leetcode.cn id=58 lang=javascript
@@ -17,7 +17,24 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    return s.trim().split(' ').pop();
+  let j = s.length -1;
+  let sum = 0;
+  let start = false; // 标记位
+  while(j>=0){
+    if(s[j]== ' '){  
+      if(start){ // 最终停止的地方
+        return sum;
+      }
+    }else{
+      start = true;
+      sum++
+    }
+    j--;
+  }
+  return sum;
 };
+// var lengthOfLastWord = function(s) {
+//     return s.trim().split(' ').pop();
+// };
 // @lc code=end
 
