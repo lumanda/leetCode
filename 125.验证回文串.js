040c02ed932c6dc00601b7e47a06d6291e6d96a5
lumanda <854260059@qq.com>
2022-02-08 15:39:11 +0800

    125,参考后第二次提交

--- 125.验证回文串.js
+++ 125.验证回文串.js
@@ -3,7 +3,7 @@
  * @version: data
  * @Author: @zhanghuiyong
  * @Date: 2022-02-08 14:42:55
- * @LastEditTime: 2022-02-08 15:23:05
+ * @LastEditTime: 2022-02-08 15:34:49
  */
 /*
  * @lc app=leetcode.cn id=125 lang=javascript
@@ -16,22 +16,22 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
+ var isPalindrome = function(s) {
   // 特殊处理空的情况 ''
   if(s.length==0) return true
-  
+  s = s.toLowerCase(); // 变为小写
   let i = 0;
   let j = s.length-1;
   while(i<j){
-    if(checkchar(s[i])){
+    if(!checkchar(s[i])){
       i++;
       continue;
     }
-    if(checkchar(s[j])){
+    if(!checkchar(s[j])){
       j--;
       continue;
     }
-    if(character(s[i]) == character(s[j])){
+    if(s[i] == s[j]){
       i++;
       j--;
     }else{
@@ -40,21 +40,51 @@ var isPalindrome = function(s) {
   }
   return true;
 };
-// 校验大小写
-function character(sub){
-  var code = sub.charCodeAt()
-  if(code>=65&&code<=90){
-    return code + 32
-  }
-  return code;
-}
+
 // 校验是否是数字或字母
 function checkchar(sub){
-  var code = sub.charCodeAt()
-  if(code>=65&&code<=90||code>=97&&code<=122||code>=48&&code<=57){
-    return false
-  }
-  return true;
+  return sub>='a'&&sub<='z'||sub>='0'&&sub<='9'
 }
+
+// var isPalindrome = function(s) {
+//   // 特殊处理空的情况 ''
+//   if(s.length==0) return true
+  
+//   let i = 0;
+//   let j = s.length-1;
+//   while(i<j){
+//     if(checkchar(s[i])){
+//       i++;
+//       continue;
+//     }
+//     if(checkchar(s[j])){
+//       j--;
+//       continue;
+//     }
+//     if(character(s[i]) == character(s[j])){
+//       i++;
+//       j--;
+//     }else{
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+// // 校验大小写
+// function character(sub){
+//   var code = sub.charCodeAt()
+//   if(code>=65&&code<=90){
+//     return code + 32
+//   }
+//   return code;
+// }
+// // 校验是否是数字或字母
+// function checkchar(sub){
+//   var code = sub.charCodeAt()
+//   if(code>=65&&code<=90||code>=97&&code<=122||code>=48&&code<=57){
+//     return false
+//   }
+//   return true;
+// }
 // @lc code=end
 
