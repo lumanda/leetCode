20c9ef30e0477723735df75a668d59dc2d8f792d
lumanda <854260059@qq.com>
2022-02-08 17:06:53 +0800

    26,第二次的优化

--- 26.删除有序数组中的重复项.js
+++ 26.删除有序数组中的重复项.js
@@ -3,7 +3,7 @@
  * @version: data
  * @Author: @zhanghuiyong
  * @Date: 2022-02-08 16:45:29
- * @LastEditTime: 2022-02-08 17:01:56
+ * @LastEditTime: 2022-02-08 17:06:27
  */
 /*
  * @lc app=leetcode.cn id=26 lang=javascript
@@ -19,15 +19,27 @@
 var removeDuplicates = function(nums) {
   let i=0,j=1;
   while(j<nums.length){
-    // 如果和前一个相等
-    if(nums[j] == nums[j-1]){
-      j++
-    }else{
-      // 如何和前一个不相等
-      nums[++i] = nums[j++];
+    // 和前一个不相等
+    if(nums[j] !== nums[j-1]){
+      nums[++i] = nums[j];
     }
+    j++
   }
   return i+1;
 };
+
+// var removeDuplicates = function(nums) {
+//   let i=0,j=1;
+//   while(j<nums.length){
+//     // 如果和前一个相等
+//     if(nums[j] == nums[j-1]){
+//       j++
+//     }else{
+//       // 如何和前一个不相等
+//       nums[++i] = nums[j++];
+//     }
+//   }
+//   return i+1;
+// };
 // @lc code=end
 
