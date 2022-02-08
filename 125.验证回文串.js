37fa2b2fb9fe5eb67ce02eb5e37d21e702b20eeb
lumanda <854260059@qq.com>
2022-02-08 15:46:17 +0800

    123,第三次利用正则提交

--- 125.验证回文串.js
+++ 125.验证回文串.js
@@ -3,7 +3,7 @@
  * @version: data
  * @Author: @zhanghuiyong
  * @Date: 2022-02-08 14:42:55
- * @LastEditTime: 2022-02-08 15:34:49
+ * @LastEditTime: 2022-02-08 15:45:18
  */
 /*
  * @lc app=leetcode.cn id=125 lang=javascript
@@ -17,34 +17,43 @@
  * @return {boolean}
  */
  var isPalindrome = function(s) {
-  // 特殊处理空的情况 ''
-  if(s.length==0) return true
-  s = s.toLowerCase(); // 变为小写
+  s = s.replace(/[^0-9a-zA-Z]/g, "").toLowerCase();
   let i = 0;
   let j = s.length-1;
   while(i<j){
-    if(!checkchar(s[i])){
-      i++;
-      continue;
-    }
-    if(!checkchar(s[j])){
-      j--;
-      continue;
-    }
-    if(s[i] == s[j]){
-      i++;
-      j--;
-    }else{
-      return false;
-    }
+    if(s[i++] !== s[j--]) return false;
   }
   return true;
 };
+//  var isPalindrome = function(s) {
+//   // 特殊处理空的情况 ''
+//   if(s.length==0) return true
+//   s = s.toLowerCase(); // 变为小写
+//   let i = 0;
+//   let j = s.length-1;
+//   while(i<j){
+//     if(!checkchar(s[i])){
+//       i++;
+//       continue;
+//     }
+//     if(!checkchar(s[j])){
+//       j--;
+//       continue;
+//     }
+//     if(s[i] == s[j]){
+//       i++;
+//       j--;
+//     }else{
+//       return false;
+//     }
+//   }
+//   return true;
+// };
 
-// 校验是否是数字或字母
-function checkchar(sub){
-  return sub>='a'&&sub<='z'||sub>='0'&&sub<='9'
-}
+// // 校验是否是数字或字母
+// function checkchar(sub){
+//   return sub>='a'&&sub<='z'||sub>='0'&&sub<='9'
+// }
 
 // var isPalindrome = function(s) {
 //   // 特殊处理空的情况 ''
