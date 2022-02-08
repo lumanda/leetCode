8515bdad4a1b40b4995cbf89226f736e63298fd0
lumanda <854260059@qq.com>
2022-02-08 17:03:45 +0800

    26,第一次提交

---
+++ 26.删除有序数组中的重复项.js
@@ -0,0 +1,33 @@
+/*
+ * @Descripttion: 
+ * @version: data
+ * @Author: @zhanghuiyong
+ * @Date: 2022-02-08 16:45:29
+ * @LastEditTime: 2022-02-08 17:01:56
+ */
+/*
+ * @lc app=leetcode.cn id=26 lang=javascript
+ *
+ * [26] 删除有序数组中的重复项
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  let i=0,j=1;
+  while(j<nums.length){
+    // 如果和前一个相等
+    if(nums[j] == nums[j-1]){
+      j++
+    }else{
+      // 如何和前一个不相等
+      nums[++i] = nums[j++];
+    }
+  }
+  return i+1;
+};
+// @lc code=end
+
