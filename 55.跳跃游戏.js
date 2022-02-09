b5bcb6a3fc9421f861cfb1eddaaf70b91559b6d9
lumanda <854260059@qq.com>
2022-02-09 09:31:15 +0800

    55,第一次提交，通过维护一个数组来记录每个位置是否可达

---
+++ 55.跳跃游戏.js
@@ -0,0 +1,39 @@
+/*
+ * @Descripttion: 
+ * @version: data
+ * @Author: @zhanghuiyong
+ * @Date: 2022-02-08 17:17:33
+ * @LastEditTime: 2022-02-09 09:31:10
+ */
+/*
+ * @lc app=leetcode.cn id=55 lang=javascript
+ *
+ * [55] 跳跃游戏
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+// 维护一个可达数组，记录每一个位置是否可达
+// 如果可达就可以从这里触发
+var canJump = function(nums) {
+  let len = nums.length
+  let i = 0;
+  let cacheArray = [1];
+  while(i<=len-1&&cacheArray[i]==1){
+    let temp = nums[i]
+    while(temp>=0){
+      if(temp+i>=len - 1) return true;
+      cacheArray[temp+i] = 1;
+      temp--
+    }
+    i++
+  }
+  return false;
+};
+console.log(canJump([3,2,1,0,4]))
+
+// @lc code=end
+
